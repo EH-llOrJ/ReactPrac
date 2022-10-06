56007831a1ba8ce7073d2944c6c9b92692072a67
EH-llOrJ <jth4115@naver.com>
2022-10-06 10:37:48 +0900

    redux설치 react-redux설치 Provider 세팅까지 연습상태

---
+++ 221006/test/src/redux/reducer/index.js
@@ -0,0 +1,15 @@
+let init = {
+  count: 0,
+};
+
+function reducer(state = init, action) {
+  switch (action) {
+    case action:
+      break;
+
+    default:
+      break;
+  }
+}
+
+export default reducer;
