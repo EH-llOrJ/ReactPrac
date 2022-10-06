b0655dddfdff5a2062cec1d098475543cd34e56d
EH-llOrJ <jth4115@naver.com>
2022-10-06 11:09:27 +0900

    thunk적용 및 미들웨어 적용(applyMiddleward)

--- 221006/test/src/redux/store.js
+++ 221006/test/src/redux/store.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import reducer from "./reducer";
 
-let store = createStore(reducer);
+// applyMiddleware 미들웨어를 적용시켜주는 함수
+// applyMiddleware() 매개변수로 적용시킬 미들웨어 매개변수로 전달
+
+let store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
