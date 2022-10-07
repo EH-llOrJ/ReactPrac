8cfdfd8da013e612d8ae90c339c74e3fb13a6ffc
EH-llOrJ <jth4115@naver.com>
2022-10-07 17:01:55 +0900

    login redirect 기능 추가

--- 221007/front/src/redux/reducer/loginReducer.js
+++ 221007/front/src/redux/reducer/loginReducer.js
@@ -8,11 +8,11 @@ function reducer(state = init, action) {
   const { type, payload } = action;
   switch (type) {
     case "LOGIN":
-      console.log("여긴 로그인");
+      console.log("로그인(리듀서 부분)");
       return { ...state, id: payload.id, pw: payload.pw, isLogin: true };
 
     case "LOGOUT":
-      console.log("여긴 로그아웃");
+      console.log("로그아웃(리듀서 부분)");
       return { ...state, id: "", pw: "", isLogin: false };
 
     default:
