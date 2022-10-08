8ae0b27801ba4a522f4655bb633043ed2b2fda45
EH-llOrJ <jth4115@naver.com>
2022-10-08 20:37:26 +0900

    220915 주석 수정

--- 220915/myapp/src/App.js
+++ 220915/myapp/src/App.js
@@ -3,7 +3,7 @@ import Block from "./components/Block";
 import { img1 } from "./img";
 
 function App() {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="App">
       {/* 배열의 갯수만큼 반복하면서 컴포넌트 생성 및 props 전달 */}
